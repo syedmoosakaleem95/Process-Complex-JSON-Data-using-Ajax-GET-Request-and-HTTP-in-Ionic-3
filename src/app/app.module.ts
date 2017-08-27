40bc849fa75a3122db6fc69c9f9e2ffae1bd497e
syed moosa kaleem <syedmoosakaleem95@gmail.com>
2017-08-27 11:43:27 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { RegisterPage } from '../pages/register/register';
 import { LoginPage } from '../pages/login/login';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
 import {HttpModule} from '@angular/http';
-import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-service';
-import { AngularFireAuthModule } from 'angularfire2/auth';
 
-
-const firebase = {
-    apiKey: "AIzaSyC5FzYKak13LLQ00pUGjqBEOCSHZlJWoIs",
-    authDomain: "bismillah-ybm.firebaseapp.com",
-    databaseURL: "https://bismillah-ybm.firebaseio.com",
-    projectId: "bismillah-ybm",
-    storageBucket: "bismillah-ybm.appspot.com",
-    messagingSenderId: "689954313399"
-};
 
 @NgModule({
   declarations: [
@@ -33,9 +20,6 @@ const firebase = {
     BrowserModule,
     HttpModule,
     IonicModule.forRoot(MyApp),
-    AngularFireDatabaseModule,
-    AngularFireModule.initializeApp(firebase),
-    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -46,10 +30,7 @@ const firebase = {
   providers: [
     StatusBar,
     SplashScreen,
-    // auth != null
-    FirebaseServiceProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    FirebaseServiceProvider
   ]
 })
 export class AppModule {}
